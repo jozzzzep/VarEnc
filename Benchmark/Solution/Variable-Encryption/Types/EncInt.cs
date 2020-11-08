0cf1f22bc6f2bfc0fea6b9e133eeb8afc1f7b83c
JosepeDev <yosefd99@gmail.com>
2020-11-08 13:04:31 +0200

    Added solution

---
+++ Benchmark/Solution/Variable-Encryption/Types/EncInt.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+public struct EncInt
+{
+    /// A struct for storing a 32-bit integer while efficiently keeping it encrypted in the memory.
+    /// In the memory it is saved as a floating-point number that is affected by random values. { encryptionKey1 & encryptionKey2 }
+    /// Every time the value changes, the encryption keys change too. And it works exactly as an int.
+    ///
+    /// Wiki page: https://github.com/JosepeDev/Variable-Encryption/wiki
+
+    #region Content
+
+    #region Encryption Key Generator
+
+    // The Random class for getting the random numbers
+    static private Random random = new Random();
+
+    // Returns a random double between 1 and 10
+    public static double GetEncryptionKey()
+    {
+        return (random.NextDouble() * 10);
+    }
+
+    #endregion
+
+    #region Variables
+
+    // The encryption values
+    private double encryptionKey1;
+    private double encryptionKey2;
+
+    // The encrypted value stored in memory
+    private double encryptedValue;
+
+    // The decrypted value
+    private double Value
+    {
+        set
+        {
+            encryptedValue = encrypt(value);
+        }
+        get
+        {
+            return Math.Round(decrypt(encryptedValue));
+        }
+    }
+
+    #endregion
+
+    #region Constructors
+
+    public static EncInt NewEncInt(int value)
+    {
+        EncInt theEncInt = new EncInt
+        {
+            encryptionKey1 = GetEncryptionKey(),
+            encryptionKey2 = GetEncryptionKey(),
+            Value = value
+        };
+        return theEncInt;
+    }
+
+    #endregion
+
+    #region Methods
+
+    // Takes a given value and returns it encrypted
+    private double encrypt(double value)
+    {
+        double valueToReturn = value;
+        valueToReturn += encryptionKey1;
+        valueToReturn *= encryptionKey2;
+        return valueToReturn;
+    }
+
+    // Takes an encrypted value and returns it decrypted
+    private double decrypt(double value)
+    {
+        double valueToReturn = value;
+        valueToReturn /= encryptionKey2;
+        valueToReturn -= encryptionKey1;
+        return valueToReturn;
+    }
+
+    // Returns the stored value as a string
+    public override string ToString()
+    {
+        return ((int)Value).ToString();
+    }
+
+    #endregion
+
+    #region Operators Overloading
+
+    /// + - * / %
+    public static EncInt operator +(EncInt eint1, EncInt eint2) => EncInt.NewEncInt((int)(eint1.Value + eint2.Value));
+    public static EncInt operator -(EncInt eint1, EncInt eint2) => EncInt.NewEncInt((int)(eint1.Value - eint2.Value));
+    public static EncInt operator *(EncInt eint1, EncInt eint2) => EncInt.NewEncInt((int)(eint1.Value * eint2.Value));
+    public static EncInt operator /(EncInt eint1, EncInt eint2) => EncInt.NewEncInt((int)(eint1.Value / eint2.Value));
+    public static EncInt operator %(EncInt eint1, EncInt eint2) => EncInt.NewEncInt((int)(eint1.Value % eint2.Value));
+
+    public static int operator +(EncInt eint1, int eint2) => (int)eint1.Value + eint2;
+    public static int operator -(EncInt eint1, int eint2) => (int)eint1.Value - eint2;
+    public static int operator *(EncInt eint1, int eint2) => (int)eint1.Value * eint2;
+    public static int operator /(EncInt eint1, int eint2) => (int)eint1.Value / eint2;
+    public static int operator %(EncInt eint1, int eint2) => (int)eint1.Value % eint2;
+
+    /// == != < >
+    public static bool operator ==(EncInt eint1, int eint2) => (int)eint1.Value == eint2;
+    public static bool operator !=(EncInt eint1, int eint2) => (int)eint1.Value != eint2;
+    public static bool operator >(EncInt eint1, int eint2) => (int)eint1.Value > eint2;
+    public static bool operator <(EncInt eint1, int eint2) => (int)eint1.Value < eint2;
+
+    public static bool operator ==(EncInt eint1, EncInt eint2) => (int)eint1.Value == (int)eint2.Value;
+    public static bool operator !=(EncInt eint1, EncInt eint2) => (int)eint1.Value != (int)eint2.Value;
+    public static bool operator <(EncInt eint1, EncInt eint2) => (int)eint1.Value < (int)eint2.Value;
+    public static bool operator >(EncInt eint1, EncInt eint2) => (int)eint1.Value > (int)eint2.Value;
+
+    /// assign
+    public static implicit operator EncInt(int value) => EncInt.NewEncInt(value);
+    public static implicit operator int(EncInt eint1) => (int)eint1.Value;
+
+    #endregion
+
+    #endregion
+}
---
+++ Benchmark/Solution/Variable-Encryption/Types/EncLong.cs
@@ -0,0 +1,134 @@
+﻿using System;
+
+public struct EncLong
+{
+    /// A struct for storing a 64-bit integer while efficiently keeping it encrypted in the memory.
+    /// In the memory it is saved as a floating-point number that is affected by random values. { encryptionKey1 & encryptionKey2 }
+    /// Every time the value changes, the encryption keys change too. And it works exactly as an long.
+    ///
+    /// Wiki page: https://github.com/JosepeDev/Variable-Encryption/wiki
+
+    #region Content
+
+    #region Encryption Key Generator
+
+    // The Random class for getting the random numbers
+    static private Random random = new Random();
+
+    // Returns a random decimal between 1 and 10
+    public static decimal GetEncryptionKey()
+    {
+        return (decimal)(random.NextDouble() * 10);
+    }
+
+    #endregion
+
+    #region Variables
+
+    // The encryption values
+    private decimal encryptionKey1;
+    private decimal encryptionKey2;
+
+    // The encrypted value stored in memory
+    private decimal encryptedValue;
+
+    // The decrypted value
+    private decimal Value
+    {
+        set
+        {
+            encryptedValue = encrypt(value);
+        }
+        get
+        {
+            return Math.Round(decrypt(encryptedValue));
+        }
+    }
+
+    #endregion
+
+    #region Methods
+
+    public static EncLong NewEncLong(long value)
+    {
+        EncLong theEncLong = new EncLong
+        {
+            encryptionKey1 = GetEncryptionKey(),
+            encryptionKey2 = GetEncryptionKey(),
+            Value = value
+        };
+        return theEncLong;
+    }
+
+    // Takes a given value and returns it encrypted
+    private decimal encrypt(decimal value)
+    {
+        decimal valueToReturn = value;
+        valueToReturn += encryptionKey1;
+        valueToReturn *= encryptionKey2;
+        return valueToReturn;
+    }
+
+    // Takes an encrypted value and returns it decrypted
+    private decimal decrypt(decimal value)
+    {
+        decimal valueToReturn = value;
+        valueToReturn /= encryptionKey2;
+        valueToReturn -= encryptionKey1;
+        return valueToReturn;
+    }
+
+    // Returns the stored value as a string
+    public override string ToString()
+    {
+        return ((long)Value).ToString();
+    }
+
+    #endregion
+
+    #region Operators Overloading
+
+    /// + - * / %
+    public static EncLong operator +(EncLong elong1, EncLong elong2) => EncLong.NewEncLong((long)(elong1.Value + elong2.Value));
+    public static EncLong operator -(EncLong elong1, EncLong elong2) => EncLong.NewEncLong((long)(elong1.Value - elong2.Value));
+    public static EncLong operator *(EncLong elong1, EncLong elong2) => EncLong.NewEncLong((long)(elong1.Value * elong2.Value));
+    public static EncLong operator /(EncLong elong1, EncLong elong2) => EncLong.NewEncLong((long)(elong1.Value / elong2.Value));
+    public static EncLong operator %(EncLong elong1, EncLong elong2) => EncLong.NewEncLong((long)(elong1.Value % elong2.Value));
+
+    public static long operator +(EncLong elong1, long elong2) => (long)elong1.Value + elong2;
+    public static long operator -(EncLong elong1, long elong2) => (long)elong1.Value - elong2;
+    public static long operator *(EncLong elong1, long elong2) => (long)elong1.Value * elong2;
+    public static long operator /(EncLong elong1, long elong2) => (long)elong1.Value / elong2;
+    public static long operator %(EncLong elong1, long elong2) => (long)elong1.Value % elong2;
+
+    public static int operator +(EncLong elong1, int elong2) => (int)elong1.Value + elong2;
+    public static int operator -(EncLong elong1, int elong2) => (int)elong1.Value - elong2;
+    public static int operator *(EncLong elong1, int elong2) => (int)elong1.Value * elong2;
+    public static int operator /(EncLong elong1, int elong2) => (int)elong1.Value / elong2;
+    public static int operator %(EncLong elong1, int elong2) => (int)elong1.Value % elong2;
+
+    /// == != < >
+    public static bool operator ==(EncLong elong1, int elong2) => (int)elong1.Value == elong2;
+    public static bool operator !=(EncLong elong1, int elong2) => (int)elong1.Value != elong2;
+    public static bool operator >(EncLong elong1, int elong2) => (int)elong1.Value > elong2;
+    public static bool operator <(EncLong elong1, int elong2) => (int)elong1.Value < elong2;
+
+    public static bool operator ==(EncLong elong1, long elong2) => (long)elong1.Value == elong2;
+    public static bool operator !=(EncLong elong1, long elong2) => (long)elong1.Value != elong2;
+    public static bool operator >(EncLong elong1, long elong2) => (long)elong1.Value > elong2;
+    public static bool operator <(EncLong elong1, long elong2) => (long)elong1.Value < elong2;
+
+    public static bool operator ==(EncLong elong1, EncLong elong2) => (long)elong1.Value == (long)elong2.Value;
+    public static bool operator !=(EncLong elong1, EncLong elong2) => (long)elong1.Value != (long)elong2.Value;
+    public static bool operator <(EncLong elong1, EncLong elong2) => (long)elong1.Value < (long)elong2.Value;
+    public static bool operator >(EncLong elong1, EncLong elong2) => (long)elong1.Value > (long)elong2.Value;
+
+    /// assign
+    public static implicit operator EncLong(long value) => EncLong.NewEncLong(value);
+    public static implicit operator long(EncLong elong1) => (long)elong1.Value;
+    public static implicit operator int(EncLong elong1) => (int)elong1.Value;
+
+    #endregion
+
+    #endregion
+}
