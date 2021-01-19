bbf0c245b2f7bb2b9d94971bf6bb9789dae5ac75
JosepeDev <yosefd99@gmail.com>
2021-01-19 17:53:09 +0200

    Update EncBool.cs
    THIS CLASS IS A JOKE
    - Updated comments
    - Minor code changes

--- EncTypes/EncBool.cs
+++ EncTypes/EncBool.cs
@@ -10,17 +10,17 @@ namespace EncTypes
 
         #region Variables
 
-        readonly bool _value;
+        // The encrypted bool
+        readonly bool _encryptedValue;
 
         #endregion
 
         #region Properties
 
-        public bool Value
-        {
-            get => Decrypt();
-        }
+        // The decrypted value of the bool
+        private bool Decrypt { get => !_encryptedValue; }
 
+        // Boolean properties
         public static string FalseString { get => Boolean.FalseString; }
         public static string TrueString { get => Boolean.TrueString; }
 
@@ -28,43 +28,38 @@ namespace EncTypes
 
         #region Constructors
 
-        private EncBool(bool value) => _value = Encrypt(value);
+        // A private constructor, used by implicit and explicit operators
+        private EncBool(bool value) => _encryptedValue = Encrypt(value);
 
         #endregion
 
         #region Methods
 
-        static bool Encrypt(bool boolVar)
-        {
-            return !boolVar;
-        }
+        // A method that takes a boolean and returns it encrypted
+        static bool Encrypt(bool boolVar) => !boolVar;
 
-        bool Decrypt()
-        {
-            return !_value;
-        }
-
-        public int CompareTo(Boolean value) => Value.CompareTo(value);
-        public int CompareTo(object obj) => Value.CompareTo(obj);
-        public Boolean Equals(Boolean obj) => Value.Equals(obj);
-        public override Boolean Equals(object obj) => Value.Equals(obj);
-        public override int GetHashCode() => Value.GetHashCode();
-        public TypeCode GetTypeCode() => Value.GetTypeCode();
-        public override string ToString() => Value.ToString();
-        public string ToString(IFormatProvider provider) => Value.ToString(provider);
+        // Boolean methods
+        public int CompareTo(Boolean value) => Decrypt.CompareTo(value);
+        public int CompareTo(object obj) => Decrypt.CompareTo(obj);
+        public Boolean Equals(Boolean obj) => Decrypt.Equals(obj);
+        public override Boolean Equals(object obj) => Decrypt.Equals(obj);
+        public override int GetHashCode() => Decrypt.GetHashCode();
+        public TypeCode GetTypeCode() => Decrypt.GetTypeCode();
+        public override string ToString() => Decrypt.ToString();
+        public string ToString(IFormatProvider provider) => Decrypt.ToString(provider);
 
         #endregion
 
         #region Operators Overloading
 
-        public static bool operator ==(EncBool eint1, bool eint2) => eint1.Value == eint2;
-        public static bool operator !=(EncBool eint1, bool eint2) => eint1.Value != eint2;
+        public static bool operator ==(EncBool eint1, bool eint2) => eint1.Decrypt == eint2;
+        public static bool operator !=(EncBool eint1, bool eint2) => eint1.Decrypt != eint2;
 
-        public static bool operator ==(EncBool eint1, EncBool eint2) => eint1.Value == eint2.Value;
-        public static bool operator !=(EncBool eint1, EncBool eint2) => eint1.Value != eint2.Value;
+        public static bool operator ==(EncBool eint1, EncBool eint2) => eint1.Decrypt == eint2.Decrypt;
+        public static bool operator !=(EncBool eint1, EncBool eint2) => eint1.Decrypt != eint2.Decrypt;
 
         public static implicit operator EncBool(bool value) => new EncBool(value);
-        public static implicit operator bool(EncBool eint1) => eint1._value;
+        public static implicit operator bool(EncBool eint1) => eint1._encryptedValue;
 
         #endregion
     }
